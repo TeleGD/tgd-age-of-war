b4c273b9cb3ab3865866a39e16d9bceb8f952e37
Clémence Picquet <clemence.picquet@etu.univ-lorraine.fr>
2018-01-26 21:05:04 +0100

    Block.java

--- src/game2/Block.java
+++ src/game2/Block.java
@@ -6,6 +6,9 @@ import org.newdawn.slick.SlickException;
 public class Block {
 	private int posx;
 	private int posy;
+	private int angle;
+	private int vitesse;
+	private boolean hit;
 	
 	//sprite
 	private Image sprite;
@@ -14,6 +17,13 @@ public class Block {
 		posx = 0;
 		posy = 0;
 		sprite = new Image(url);
+		angle = 0;
+		vitesse = 0;
+		hit = false;
+	}
+	
+	public void testHit(){
+		
 	}
 	
 	public int getPosx(){
@@ -28,6 +38,14 @@ public class Block {
 		return sprite;
 	}
 	
+	public int getAngle(){
+		return angle;
+	}
+	
+	public int getVitesse(){
+		return vitesse;
+	}
+	
 	public void setPosx(int x){
 		posx = x;
 	}
@@ -38,5 +56,13 @@ public class Block {
 	
 	public void setSprite(String url) throws SlickException{
 		sprite = new Image(url);
+	}
+	
+	public void setAngle(int ang){
+		angle = ang;
+	}
+	
+	public void setVitesse(int fast){
+		vitesse = fast;
 	}
 }
