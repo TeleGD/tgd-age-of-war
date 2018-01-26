4296ea4ecbad140788360c0a3e7eafe6fc4ac5fe
Clémence Picquet <clemence.picquet@etu.univ-lorraine.fr>
2018-01-26 23:34:46 +0100

    World2.java

--- src/game2/World2.java
+++ src/game2/World2.java
@@ -54,14 +54,14 @@ public class World2 extends BasicGameState {
     	dieu.render(container, game, g);
     	
     	g.setColor(Color.white);
-    	g.drawString("Time : " + time/1000 + "h" + time/1000 + "min" + time/1000 + "s", 1100, 100);
+    	g.drawString("Time : " + ((time/1000)/60)/60 + "h " + (time/1000)/60 + "min " + ((time/1000)%60)%60  + "s", 1110, 100);
     	
     }
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
     	dieu.update(container, game, delta);
-    	//player.update(container, game, delta);
+    	player.update(container, game, delta);
     	time += delta;
     }
     
