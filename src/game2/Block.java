c52a62d61565104ff04392f217001a5dce5bf837
Clémence Picquet <clemence.picquet@etu.univ-lorraine.fr>
2018-01-26 20:22:36 +0100

    Block.java

---
+++ src/game2/Block.java
@@ -0,0 +1,21 @@
+package game2;
+
+public class Block {
+	private int posx;
+	private int posy;
+	
+	public Block(){
+		posx = 0;
+		posy = 0;
+	}
+	
+	public int getPosx(){
+		return posx;
+	}
+	
+	public int getPosy(){
+		return posy;
+	}
+	
+	
+}
