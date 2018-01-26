0936a80383fc876cf9abc1a905a97ba17763aec0
ElMrini Alexis <alexis.el-mrini@telecomnancy.eu>
2018-01-26 22:35:46 +0100

    modificaiton constructeur Board

--- src/aow/Board.java
+++ src/aow/Board.java
@@ -12,35 +12,41 @@ public class Board {
 	private int[] damier;
 	private int case_width;  //largeur d'une case  
 	private int left_border; //pixel de démarrage à gauche du tableau
+	private int right_border;
+	private int damier_width;
 	
-
 	
 	
 	/********* constructeur ********/
 	
 	
-	public Board(int size, int case_width, int left_border, int right_border){
+	public Board(int size, int left_border, int right_border){
 		/*
 		 * le nombre de cases (size) du damier est défini dans le Main.
-		 * left_border : position du bord gauche
-		 * case_width : largeur d'une case
+		 * left_border : position du bord gauche.
+		 * right_border : position du bord droit.
 		 * 
 		 */
 		damier = new int[size];
 		Arrays.fill(damier, 0);
-		this.case_width = case_width;
+		
 		this.left_border = left_border;
+		this.right_border = right_border;
+		this.damier_width = right_border-left_border;
+		this.case_width = (int) this.damier_width/size;
+		
 		
 	}
 	
 	
 	/******* calculs de positions ******/
 	
+	
 	public int getCorner(int index){
 		/*
 		 * retourne la position horizontale de la case d'indice index
 		 */
-		return index*this.case_width+left_border;
+		return index*this.case_width+this.left_border;
 		
 	}
 	
@@ -51,12 +57,16 @@ public class Board {
 	
 	public void setMinionToCase(int player, int index){
 		/*
-		 * méthode pour attribuer 
+		 * méthode pour attribuer à une case l'ID d'un joueur 
+		 * en fonction du minion présent sur la case
 		 */
 		this.damier[index] = player;
 	}
 	
 	public void setVoidToCase(int index){
+		/*
+		 * méthode pour rendre une case vide
+		 */
 		this.damier[index] = 0;
 	}
 	
