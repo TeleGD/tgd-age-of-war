1d6bc9cdc626c6c96e2a04e91acad929c024daaf
Philippe <philippe.graff@telecomnancy.net>
2018-01-26 23:47:18 +0100

    ajout de render pour la classe joueur

--- src/aow/entity/Player.java
+++ src/aow/entity/Player.java
@@ -1,14 +1,22 @@
 package aow.entity;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
 public class Player {
 	
 	
 	
 	private int PV; //pts de vie de la base
+	private int PVMax;
 	private int gold;
 	private int ID; //num joueur
 	private int xp;
+	private int xpMax;
+	private int age;
 	
 	//constructeur
 	public Player(int num, int gold, int HP ) // init num joueur, or et pv 
@@ -16,6 +24,7 @@ public class Player {
 		ID=num;
 		this.gold=gold;
 		PV=HP;
+		PVMax=HP;
 	}
 	
 	
@@ -30,6 +39,12 @@ public class Player {
 		return ID;
 	}
 	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	
 	
 	public boolean removeGold(int toRemove) // enleve toRemove a l or du joueur, retourne true si le joueur peut payer
 	{
@@ -42,14 +57,49 @@ public class Player {
 		}
 	}
 	
-	
-	
 	public void augmenteXp(int inc)
 	{
 		xp+=inc;
 	}
+	 
+	public void update(GameContainer container,StateBasedGame game, int delta) throws SlickException
+	{
+		// update de age, xpMax, pv, pvmax ;
+		if(xp>=xpMax)
+		{
+			age++;
+			xpMax=(int)(xpMax*1.75);
+			PVMax=(int)(PVMax*1.25);
+			PV=(int)(PV*1.25);
+		}
+		
+	}
 	
-	
+	public void render(GameContainer container,StateBasedGame game, Graphics g) throws SlickException
+	{
+		if(ID==1)
+		{
+			g.setColor(new Color(153,0,0));
+			g.fillRect(6, 247, 120, 18);
+		
+			g.setColor(new Color(255,0,0));
+			g.fillRect(6, 247, 120*(int)(PV/PVMax), 18);
+			
+			g.drawString(""+((double)(PV)/PVMax)*100, 6, 247);
+		}
+		
+		
+		else //deux joueur
+		{
+			g.setColor(new Color(153,0,0));
+			g.fillRect(594, 247, 120, 18); //
+			
+			g.setColor(new Color(255,0,0));
+			g.fillRect(594, 247, 120*(int)(PV/PVMax), 18);
+			
+			g.drawString(""+((double)(PV)/PVMax)*100, 594, 247);
+		}
+	}
 	
 	
 }
