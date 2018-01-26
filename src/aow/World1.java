ffa288feb893cbfba71886f040db1f024f46d5ac
GEORGE Amos <amos.george@telecomnancy.eu>
2018-01-26 22:08:45 +0100

    Initialisation Player

--- src/aow/World1.java
+++ src/aow/World1.java
@@ -10,13 +10,22 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
+
+import aow.entity.Player;
+
 import java.util.*;
 
 public class World1 extends BasicGameState {
 	
 	public static int ID=7;
 	public static String name = "Age of War";
-
+	
+	public int goldInit = 50;
+	public int HPInit = 100;
+	
+	public Player p1 = new Player(1,goldInit,HPInit);
+	public Player p2 = new Player(2,goldInit,HPInit);
+	
     @Override
     public void init(final GameContainer container, final StateBasedGame game) throws SlickException {
 
