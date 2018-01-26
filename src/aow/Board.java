e45a54f3affa6a8c1656596df8f60aea30a97ca0
ElMrini Alexis <alexis.el-mrini@telecomnancy.eu>
2018-01-27 00:35:26 +0100

    Board : ajout du rafraichissement de position

--- src/aow/Board.java
+++ src/aow/Board.java
@@ -1,5 +1,7 @@
 package aow;
 import java.util.Arrays;
+import java.util.Random;
+
 
 public class Board {
 /*
@@ -12,8 +14,14 @@ public class Board {
 	private int[] damier;
 	private int case_width;  //largeur d'une case  
 	private int left_border; //pixel de démarrage à gauche du tableau
-	private int right_border;
-	private int damier_width;
+	private int right_border;  //pixel de fin du tableau à droite
+	private int damier_width;   //taille en pixels du tableau
+	private int lead_1; //position du minion leader du joueur 1
+	private int lead_2; //position du minion leader du joueur 2
+	private Random r;
+	
+	
+	/********* Getters *******/
 	
 	
 	
@@ -30,19 +38,21 @@ public class Board {
 		damier = new int[size];
 		Arrays.fill(damier, 0);
 		
+		
 		this.left_border = left_border;
 		this.right_border = right_border;
 		this.damier_width = right_border-left_border;
 		this.case_width = (int) this.damier_width/size;
-		
-		
+		this.lead_1 = 0;
+		this.lead_2 = size-1;
+		this.r = new Random();
 	}
 	
 	
 	/******* calculs de positions ******/
 	
 	
-	public int getCorner(int index){
+	public int getX(int index){
 		/*
 		 * retourne la position horizontale de la case d'indice index
 		 */
@@ -56,21 +66,99 @@ public class Board {
 	/********* modification du damier ******/
 	
 	public void setMinionToCase(int player, int index){
-		/*
+		/* 
+		 * 
 		 * méthode pour attribuer à une case l'ID d'un joueur 
 		 * en fonction du minion présent sur la case
+		 * 
 		 */
 		this.damier[index] = player;
 	}
 	
 	public void setVoidToCase(int index){
-		/*
+		/* 
+		 * 
 		 * méthode pour rendre une case vide
+		 * 
 		 */
 		this.damier[index] = 0;
 	}
 	
 	
+	/****** fonctions de rafraichissement du damier ******/
+	
+	
+	public void refreshPositions(){
+		
+		/* calcule le nouveau damier à partir de l'ancien */
+		
+		
+		int n = this.damier.length;
+		
+		/* On recalcule au préalable les positions des deux leaders,
+		 * au cas où des minions ont disparus ou sont apparus.
+		 */
+		
+		this.lead_1=0;
+		this.lead_2=n-1;
+		int i = 0;
+		while(this.damier[i]!=2){
+			if(this.damier[i] == 1){
+				this.lead_1 = i;
+			}
+			i++;
+		}
+		this.lead_2 = i;
+		
+		/*
+		 *  on traite d'abord le cas particulier où les leaders
+		 *  se départagent une même case. 
+		 *  Cas sur les bords : priorité aux minions déjà dans leurs bases
+		 *  Cas général : aléatoire
+		 */
+		
+		if(this.lead_2 == this.lead_1 + 2){
+			if(this.lead_1 == 0){ //Cas où le lead 1 est juste à côté de sa base
+				this.damier[0] = 0;
+				this.damier[1] = 1;
+			}else if (this.lead_2 == n-1){ //Cas où le lead 2 est juste à côté de sa base
+				this.damier[n-1] = 0;
+				this.damier[n-2] = 2;
+			}else{ //Si on est quelque part au milieu, un des leaders au hasard avance
+				int c = r.nextInt(2)+1;
+				this.damier[this.lead_1+1]=c;
+				if(c==1){
+					this.damier[this.lead_1] = 0;
+				}else{
+					this.damier[this.lead_2] = 0;
+				}
+			}
+			
+		}
+		
+		
+		/* On part ensuite de chaque leader et on remonte jusqu'à la base, 
+		 * afin d'éviter que les unités leaders bloquent leur prédécesseurs 
+		 */
+		
+		/* on commence en mettant à jour les minions du joueur 1 */
+		for(int j = this.lead_1;j>0;j--){
+			if(this.damier[j] == 1 && j != n-3 && this.damier[j+1] == 0){
+				this.damier[j] = 0;
+				this.damier[j+1] = 1;
+			}
+		}
+		
+		/* on met ensuite à jour les minions du joueur 2 */
+		for(int j = this.lead_2;j< 0;j++){
+			if(this.damier[j] == 2 && j != 2 && this.damier[j-1] == 0){
+				this.damier[j] = 0;
+				this.damier[j-1] = 2;
+			}
+		}
+		
+		
+	}
 	
 	
 }
