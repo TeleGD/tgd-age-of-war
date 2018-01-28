aec48b87951cca900ba3b5657990d813cb0076da
Philippe <philippe.graff@telecomnancy.net>
2018-01-28 06:14:57 +0100

    ajout de la classe sort. last commit.

--- src/aow/entity/minions/Sort.java
+++ src/aow/entity/minions/Sort.java
@@ -21,19 +21,21 @@ public class Sort {
 	private int power; // puissance du sort
 	private int numJoueur; // de quel cote est le sort
 	private Image picture; // les images du sort associees à chaque epoque 
-	private int x,y; // ira jusqu'au point culminant centre
-	private int pente=(15-1000)/(int)((World1.board.getX(World1.tailleBoard-1)-World1.board.getX(0))/2);//difference de hauteur / longueur axe des x  ; 15 sera la hauteur max
+	private double x,y; // ira jusqu'au point culminant centre
+	private double pente=(30.0-350.0)/((World1.board.getX(World1.tailleBoard-1)-World1.board.getX(0))/2); //difference de hauteur / longueur axe des x  ; 15 sera la hauteur max
+	private int i=0; //compteur d explosion
+	
 	
 	/* constructeur : -initialise power en fonction de l'age */
 	public Sort(int numplayer, int age)
 	{
 		numJoueur=numplayer;
-		
+		System.out.println("pente :"+pente);
 		if(age==1)
 		{
 			power=15;
 			try {
-				picture=new Image("images/game1/stick_.png"); // a faire le nom
+				picture=new Image("images/game1/rock.png"); // a faire le nom
 			} catch (SlickException e) {
 				// nous donne la trace de l'erreur si on ne peut charger l'image correctement
 				e.printStackTrace();
@@ -45,7 +47,7 @@ public class Sort {
 			{
 				power=30;
 				try {
-					picture=new Image("images/game1/stick_.png"); // a faire le nom
+					picture=new Image("images/game1/bullet.png"); // a faire le nom
 				} catch (SlickException e) {
 					// nous donne la trace de l'erreur si on ne peut charger l'image correctement
 					e.printStackTrace();
@@ -56,7 +58,7 @@ public class Sort {
 				{
 					power=45;
 					try {
-						picture=new Image("images/game1/stick_.png"); // a faire le nom
+						picture=new Image("images/game1/rocket.png"); // a faire le nom
 					} catch (SlickException e) {
 						// nous donne la trace de l'erreur si on ne peut charger l'image correctement
 						e.printStackTrace();
@@ -68,12 +70,12 @@ public class Sort {
 		if(numJoueur==1)
 		{
 			x=140;
-			y=1000;
+			y=350;
 		}
 		else
 		{
 			x=World1.board.getX(World1.tailleBoard-1)+50; // n taille du Damier
-			y=1000;
+			y=350;
 		}// initialise les coordonnees
 		
 	}
@@ -82,24 +84,22 @@ public class Sort {
 	{
 		
 		
-		if(y>15)
+		if(y>30)
 		{
 			deplace();
 		}
-		else
+		else // arrives a destination, fin de l objet
 		{
-			this.hitEnemies();
-			try {
-				picture=new Image("images/game1/stick_.png"); // a faire le nom
-			} catch (SlickException e) {
-				// nous donne la trace de l'erreur si on ne peut charger l'image correctement
-				e.printStackTrace();
+			i++;
+			if (i==24) {
+				this.hitEnemies();
 			}
 		}
 		
-		
+		if (i>63) {
+			y=-100;
+		}
 	}
-	
 	
 	
 	/* se deplace a chaque fois que on l appelle : a la fin arrivera au centre a la hauteur 15 */
@@ -121,12 +121,28 @@ public class Sort {
 	/* dedie a l affichage de image */
 	public void render(GameContainer container, StateBasedGame game, Graphics g) {
 		
-			g.drawImage(picture, (float)(x), (float)(y));
+			g.drawImage(picture, (float)(x-picture.getWidth()/2), (float)(y-picture.getHeight()/2));
+			
+			
+			if(y<=30) // explosion declenchee et des images a afficher
+			{
+				try {
+					picture=new Image("images/game1/boom"+((i/8)+1)+".png"); 
+				} catch (SlickException e) {
+					// nous donne la trace de l'erreur si on ne peut charger l'image correctement
+					e.printStackTrace();
+				}
+			}
+			}
 		
-	}
+	
 	
 
 	
+	public int getI() {
+		return i;
+	}
+
 	/* cherche les ennemis et lance l'attaque, le fera quand sera au zenith, apres avoir explose */
 	public void hitEnemies()
 	{
